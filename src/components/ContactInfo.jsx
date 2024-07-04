86a59bf06a17ace8685bf8d01b7a63d331040d59
Chao Moua <chaomoua91@gmail.com>
2024-07-04 10:55:31 -0500

    finished the contact info section

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,3 +1,73 @@
 import FormField from "/.FromField";
 
-const ContactInfo = () => {};
+const ContactInfo = ({ cvData, setCvData }) => {
+  const handleChange = (e) => {
+    setCvData({ ...cvData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="contactInfo">
+      <div className="contactHeading">
+        <h2>Contact Information</h2>
+      </div>
+      <form>
+        <div className="nameIfoInputs">
+          <FormField
+            placeholder="First Name"
+            type="text"
+            name="firstName"
+            value={cvData.firstName}
+            onChange={handleChange}
+          />
+
+          <FormField
+            placeholder="Last Name"
+            type="text"
+            name="lastName"
+            value={cvData.lastName}
+            onChange={handleChange}
+          />
+          <FormField
+            placeholder="Phone Number"
+            type="number"
+            name="phoneNumber"
+            value={cvData.phoneNumber}
+            onChange={handleChange}
+          />
+
+          <FormField
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={cvData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="contDateInput">
+          <FormField
+            type="date"
+            name="dateOfBirth"
+            value={cvData.dateOfBirth}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+      <div className="profSummery">
+        <div className="profHeading">
+          <h2>Professional Summary</h2>
+        </div>
+        <form>
+          <FormField
+            placeholder="Summary"
+            type="text"
+            name="summary"
+            value={cvData.summary}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
