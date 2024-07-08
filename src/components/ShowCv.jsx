5f320618ecc5842e23384c226b130c1e7a007288
Chao Moua <chaomoua91@gmail.com>
2024-07-08 15:52:34 -0500

    Display the CV resume

---
+++ src/components/ShowCv.jsx
@@ -0,0 +1,71 @@
+const ShowCv = ({ cvData }) => {
+  return (
+    <>
+      <nav>
+        <h2 className="cvShowHead">Name</h2>
+        <h2 className="cvShowHead">
+          {cvData.firstName} {cvData.LastName}
+        </h2>
+        <div className="contactTextHead">
+          <p>PNum: {cvData.phoneNumber}</p>
+          <p>Email: {cvData.email}</p>
+        </div>
+      </nav>
+      <section className="maincontainer">
+        <div className="perDesText">
+          <h2>Personal Description:</h2>
+
+          <div>
+            <p>Date Of Birth: {cvData.dateOfBirth}</p>
+            <p>Summary: {cvData.summary} </p>
+          </div>
+        </div>
+
+        <div className="EducationText">
+          <h2>Education:</h2>
+
+          <div className="EduTextDetail">
+            {cvData.newEducation.map((edu) => (
+              <div key={edu.id}>
+                <p>Degree: {edu.degree}</p>
+                <p>Institute: {edu.institute}</p>
+                <p>Location: {edu.location}</p>
+                <p>Completed: {edu.completed}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="workExpText">
+          <h2>Work Experience:</h2>
+
+          <div className="workTextDetail">
+            {cvData.workExperience.map((work) => (
+              <div key={work.id}>
+                <p>Title: {work.title}</p>
+                <p>Name: {work.name}</p>
+                <p>Location: {work.location}</p>
+                <p>Date: {work.date}</p>
+                <p>Description: {work.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="skillsText">
+          <h2>Skills:</h2>
+          <div className="skillsTextDetail">
+            {cvData.allSkills.map((skill) => (
+              <div key={skill.id}>
+                <p>Title: {skill.title}</p>
+                <p>Description: {skill.description}</p>
+                <p>Start Date: {skill.startDate}</p>
+                <p>End Date: {skill.endDate}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
